4e922d8c0d04897e8320b8f22ded70cf228e4c39
Tunde <babatea4real@yahoo.com>
2023-09-19 15:00:05 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// App.jsx
+
+import "./App.css";
+import MovieList from "./components/MovieList";
+import ProjectList from "./components/ProjectList";
+import SimpleList from "./components/SimpleList";
+import StudentList from "./components/StudentList";
+
+function App() {
+  return (
+    <div className="App">
+      <SimpleList />
+      <StudentList />
+      <ProjectList />
+      <MovieList />
+    </div>
+  );
+}
+
+export default App;
